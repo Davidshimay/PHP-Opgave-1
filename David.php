9ead95b7a6ac6d2b0aca149416549b5451973fa0
Davidshimay <133370621+Davidshimay@users.noreply.github.com>
2023-05-12 15:12:21 +0200

    Add files via upload

---
+++ David.php
@@ -0,0 +1,26 @@
+<?php
+$var1 = 12;
+$var2 = 10;
+$resultaat = $var1 + $var2;
+echo $resultaat;
+?>
+
+<?php
+$var1 = 12;
+$var2 = 10;
+$resultaat = $var1 * $var2;
+echo $resultaat;
+?>
+
+
+<?php
+$naam="David";
+echo "Welkom ".$naam. "!";
+?>
+
+<?php
+$mijnNaam="Carl";
+$jouwNaam="Camel"-+
+;
+echo $mijnNaam . " zit bij " . jouwNaam . " in de klas!";
+?>
